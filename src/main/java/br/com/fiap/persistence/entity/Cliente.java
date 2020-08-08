9deb92de120e82f31846226d6e80f632259d2e3f
Guilherme Moraes <guilhermemoraes@MacBook-Air-de-Guilherme.local>
2020-08-08 01:27:42 -0300

    Crud de cliente

---
+++ src/main/java/br/com/fiap/persistence/entity/Cliente.java
@@ -0,0 +1,29 @@
+package br.com.fiap.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Table(name = "tbl_cliente") @Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Long idCliente;
+
+    @Column(name = "cpf", unique = true)
+    private String cpf;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+}
